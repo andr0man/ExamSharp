6dff668515cca49f867c0912a01a4edc2fe119bb
andr0man <andriylavrenyuk12@gmail.com>
2024-07-13 22:55:22 +0300

    add export of a word and its translations to a separate file.

---
+++ Exam_С#/BlazorUI/wwwroot/saveFile.js
@@ -0,0 +1,13 @@
+﻿function saveFile(fileName, Content) {
+    var link = document.createElement('a');
+
+    link.download = fileName;
+
+    link.href = "data:text/plain;charset=utf-8," + encodeURIComponent(Content);
+
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link);
+}
